b5a417b989f0c5e4c6216e02cbc353fe4fb9e592
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2022-01-09 18:21:49 -0300

    Acrescentar estilo global. Finalizar estrutura basica do Navbar.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-// https://www.youtube.com/watch?v=Z-hACIsjv4E
-//  0hs 8' 10''  
+// api- https://www.youtube.com/watch?v=Z-hACIsjv4E
+//  0hs 8' 10'' 
+// ui- https://www.youtube.com/watch?v=vIxGDq1SPZQ
+//  0hs 21' 40''  
 import '../styles/globals.css';
 
 // components
